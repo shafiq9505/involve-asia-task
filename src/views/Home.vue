69307292457347720328f66e094fffcfc1c9da35
shafiq <shafiq@hitachi-ebworx.com>
2021-04-30 00:52:20 +0800

    Finalise

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,7 +21,7 @@
 										<div>Revenue Group Title</div>
 									</b-col>
 									<b-col class="mt-1" cols="12">
-										<b-form-input></b-form-input>
+										<b-form-input v-model="fields.revenueGroup.value"></b-form-input>
 									</b-col>
 								</b-row>
 							</b-col>
@@ -32,9 +32,8 @@
 									<b-col cols="12">
 										if
 										<span style="width: 20%">
-											<select class="form-control d-inline-block w-25 mr-1">
-												<option selected>All</option>
-												<option value="1">im not selected</option>
+											<select v-model="fields.secondField.value" @change="onSelect(fields.secondField.value)" class="form-control d-inline-block w-25 mr-1">
+												<option v-for="option in fields.secondField.options" :key="option.value" :value="option.value">{{ option.desc }}</option>
 											</select>
 										</span>
 
@@ -48,27 +47,26 @@
 								<b-row v-for="(rule, index) in fields.rules" :key="index" class="py-3">
 									<!-- first dropdown -->
 									<b-col cols="2" class="pr-1">
-										<select v-model="fields.rules[index].firstDropdown" class="form-control">
-											<option selected value="">aff_sub</option>
-											<option value="1">im not selected</option>
+										<select v-model="fields.rules[index].firstDropdown" @change="onSelect(fields.rules[index].firstDropdown)" class="form-control">
+											<option v-for="option in fields.thirdField.options" :key="option.value" :value="option.value">{{ option.desc }}</option>
 										</select>
 									</b-col>
 
 									<!-- second dropdown -->
 									<b-col cols="2" class="px-1">
-										<select v-model="fields.rules[index].secondDropdown" class="form-control">
-											<option selected value="">aff_sub</option>
-											<option value="1">im not selected</option>
+										<select v-model="fields.rules[index].secondDropdown" @change="onSelect(fields.rules[index].secondDropdown)" class="form-control">
+											<option v-for="option in fields.fourthField.options" :key="option.value" :value="option.value">{{ option.desc }}</option>
 										</select>
 									</b-col>
 
 									<!-- parameter input -->
-									<b-col cols="8" v-for="(parameter, innerIndex) in fields.rules[index].parameters" :key="innerIndex">
-										<b-row>
+									<b-col cols="8">
+										<b-row v-for="(parameter, innerIndex) in fields.rules[index].parameters" :key="innerIndex">
 											<b-col cols="8" class="px-1">
 												<b-input-group>
-													<b-form-input cols="6" v-model="fields.rules[index].parameters[innerIndex]"></b-form-input>
+													<b-form-input placeholder="insert parameter" cols="6" v-model="fields.rules[index].parameters[innerIndex]"></b-form-input>
 													<b-input-group-append>
+														<!-- Add rule -->
 														<b-button
 															@click="addParameters(index)"
 															v-show="fields.rules[index].parameters.length - 1 === innerIndex"
@@ -76,6 +74,8 @@
 															variant="outline-primary"
 															>add rule</b-button
 														>
+
+														<!-- Remove rule -->
 														<b-button
 															@click="removeParameters(index, innerIndex)"
 															v-show="fields.rules[index].parameters.length - 1 !== innerIndex"
@@ -90,12 +90,22 @@
 											<!-- Button -->
 											<b-col cols="4" class="px-1 py-1">
 												<!-- minus -->
-												<b-button @click="removeRules(index)" variant="outline-*" :disabled="fields.rules.length === 1">
+												<b-button
+													v-show="fields.rules[index].parameters.length - 1 === innerIndex"
+													@click="removeRules(index)"
+													variant="outline-*"
+													:disabled="fields.rules.length === 1"
+												>
 													<b-icon icon="dash-circle" style="width: 25px; height: 25px"></b-icon>
 												</b-button>
 
 												<!-- add -->
-												<b-button variant="outline-*" @click="addRules()" :disabled="false">
+												<b-button
+													v-show="fields.rules.length - 1 === index && fields.rules[index].parameters.length - 1 === innerIndex"
+													variant="outline-*"
+													@click="addRules()"
+													:disabled="false"
+												>
 													<b-icon style="width: 25px; height: 25px" icon="plus-circle"></b-icon>
 												</b-button>
 											</b-col>
@@ -139,8 +149,48 @@ const data = function data() {
 	return {
 		name,
 		fields: {
+			revenueGroup: {
+				value: ''
+			},
 			totalRevenue: {
 				value: '15'
+			},
+			secondField: {
+				value: '',
+				options: [
+					{
+						value: '',
+						desc: 'Please select'
+					},
+					{
+						value: 'All',
+						desc: 'ALL'
+					}
+				]
+			},
+			thirdField: {
+				options: [
+					{
+						value: '',
+						desc: 'Please select'
+					},
+					{
+						value: 'aff_sub',
+						desc: 'aff_sub'
+					}
+				]
+			},
+			fourthField: {
+				options: [
+					{
+						value: '',
+						desc: 'Please select'
+					},
+					{
+						value: 'is',
+						desc: 'is'
+					}
+				]
 			},
 			rules: [
 				{
@@ -155,6 +205,13 @@ const data = function data() {
 
 const methods = {
 	confirm() {
+		const obj = {}
+		obj.revenueGroup = this.fields.revenueGroup.value
+		obj.condtion = this.fields.secondField.value
+		obj.rules = this.fields.rules
+		obj.totalRevenue = this.fields.totalRevenue
+		console.warn(obj)
+		window.alert(JSON.stringify(obj))
 		this.$bvModal.hide('modal')
 	},
 	addRules() {
@@ -166,14 +223,17 @@ const methods = {
 		this.fields.rules.push(tempObj)
 	},
 	removeRules(index) {
-		console.warn(index)
 		this.fields.rules.splice(index, 1)
 	},
 	addParameters(index) {
 		this.fields.rules[index].parameters.push('')
 	},
 	removeParameters(index, innerIndex) {
-		console.warn(index, innerIndex)
+		this.fields.rules[index].parameters.splice(innerIndex, 1)
+	},
+	onSelect(value) {
+		// Bind into one function since no real logic
+		console.warn(value)
 	}
 }
 
